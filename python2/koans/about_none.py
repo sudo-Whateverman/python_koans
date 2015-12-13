f9aae9afd0ec252806ea0a68ad0b6cf4cbe57f7c
Nick <nick@smartgreen.co.il>
2015-12-13 11:19:15 +0200

    The enlightment is near

--- python2/koans/about_none.py
+++ python2/koans/about_none.py
@@ -37,15 +37,15 @@ class AboutNone(Koan):
             # Need a recap on how to evaluate __class__ attributes?
             #   https://github.com/gregmalcolm/python_koans/wiki/Class-Attribute
 
-            self.assertEqual(__, ex.__class__)
+            self.assertEqual(AttributeError, ex.__class__)
 
             # What message was attached to the exception?
             # (HINT: replace __ with part of the error message.)
-            self.assertMatch(__, ex.args[0])
+            self.assertMatch("'NoneType' object has no attribute 'some_method_none_does_not_know_about'", ex.args[0])
 
     def test_none_is_distinct(self):
         """
         None is distinct from other things which are False.
         """
-        self.assertEqual(____, None is not 0)
-        self.assertEqual(____, None is not False)
+        self.assertEqual(True, None is not 0)
+        self.assertEqual(True, None is not False)
